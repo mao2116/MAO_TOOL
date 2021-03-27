2ca3b0f4b37c45249821d3a859cd073e9798a6c6
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-03-27 23:46:30 +0600

    Create maotool.sh

---
+++ maotool.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#coder Ekramul Hassan
+# FB ID: 
+#If You Copy Thise Then Give Me Credit
+
+
+apt update
+
+apt upgrade
+
+apt install git
+
+apt install python
+
+apt install python2
+
+pip install pyfiglet
+
+pip install termcolor
+
+pip install requests
+
+pip install pytube
+
+python3 test.py
