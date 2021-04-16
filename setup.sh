16bc26b6c016ccbd8bd9685cbd2993db7022bb63
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-04-16 15:22:08 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+clear
+
+sleep 1.0
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pip install requests
+pip install pytube
+
+clear
+sleep 1.0
+cd $HOME/MAO_TOOL
+mv maotool maotool.py /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod +x maotool maotool.py
+cd $HOME/MAO_TOOL
+clear
+sleep 1.0
+echo -e "\033[1;94m   Now Type: maotool"
+rm -rf setup.sh
