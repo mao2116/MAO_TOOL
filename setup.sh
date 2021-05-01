d63fe1b0ad48c959516a8809707743a82f89676b
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-05-01 13:52:13 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 clear
 
 sleep 1.0
+pkg uninstall python -y
 pkg install python -y
 pkg install python2 -y
 pip install requests
@@ -9,6 +10,10 @@ pip install pytube
 
 clear
 sleep 1.0
+cd $PREFIX/usr/bin
+rm -rf maotool.py
+rm -rf maotool.pyc
+rm -rf maotool
 cd $HOME/MAO_TOOL
 mv maotool maotool.pyc /data/data/com.termux/files/usr/bin
 cd /data/data/com.termux/files/usr/bin
